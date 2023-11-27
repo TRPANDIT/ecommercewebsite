12af5eab5fd02ed3cbaf8897795c5a3bbcdef5ae
TRPANDIT <tripurari.pandit@capgemini.com>
2023-11-27 14:42:21 +0530

    first commit

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrl: './cards.component.css'
+})
+export class CardsComponent {
+  books = [
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    },
+    {
+      bookUrl:"https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/q/p/y/shri-ramcharitramanas-ramayan-book-in-large-size-deluxe-edition-original-imagge85bdqhkqwx.jpeg?q=90",
+      title: "Ramayan"
+    }
+  ]
+}
